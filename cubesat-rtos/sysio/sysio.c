1c85e9007870acb7e821e92e60418f0446ac15c7
ThePetrovich <petroff.ap@gmail.com>
2020-04-29 15:22:59 +0900

    Changed systemd file structure systemd: removed directories in source folder

---
+++ cubesat-rtos/sysio/sysio.c
@@ -0,0 +1,6 @@
+/*
+ * sysio.c
+ *
+ * Created: 28.04.2020 21:03:57
+ *  Author: Admin
+ */ 
---
+++ include/sysio/sysio.h
@@ -0,0 +1,16 @@
+/*
+ * sysio.h
+ *
+ * Created: 28.04.2020 21:05:48
+ *  Author: Admin
+ */ 
+
+
+#ifndef SYSIO_H_
+#define SYSIO_H_
+
+
+
+
+
+#endif /* SYSIO_H_ */
